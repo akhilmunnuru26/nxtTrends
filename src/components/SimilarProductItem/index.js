225ea93b16740bc6f1ab57c0f7d4a4184e1afbf0
Akhil Munnuru <104784452+akhilmunnuru26@users.noreply.github.com>
2022-10-21 10:50:02 +0530

    Update index.js

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,12 +1,29 @@
 import './index.css'
 
 const SimilarProductItem = props => {
-  const {similarProducts} = props
-  const {imageUrl} = similarProducts
+  const {children} = props
+  console.log(children)
 
   return (
-    <li>
-      <img src={imageUrl} alt="1" />
+    <li key={children.id} className="similar-product-item">
+      <img
+        className="similar-products-img"
+        src={children.imageUrl}
+        alt={children.title}
+      />
+      <p className="similar-product-title">{children.title}</p>
+      <p className="similar-product-brand">by {children.brand}</p>
+      <div className="similar-product-price-rating-container">
+        <p className="similar-product-price">Rs {children.price}/-</p>
+        <div className="similar-product-rating-section">
+          <p className="similar-product-rating-number">{children.rating}</p>
+          <img
+            src="https://assets.ccbp.in/frontend/reacchildrent-js/star-img.png"
+            alt="star"
+            className="stars-image"
+          />
+        </div>
+      </div>
     </li>
   )
 }
